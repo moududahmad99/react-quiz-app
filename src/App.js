8e6f00fc6f97b3663df99880713afbb8496c6133
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-04-26 21:48:29 +0600

    MainMenu User Interface design Complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import MainMenu from './components/MainMenu';
 import Quiz from './components/Quiz';
 import LastScreen from './components/LastScreen';
-
+import { QuizContext } from './Helpers/Contexts'
 
 function App() {
 
@@ -13,10 +13,11 @@ function App() {
     return (
         <div className='App'>
             <h1>Quiz App</h1>
-
-            {currentComponent === 'menu' && <MainMenu />}
-            {currentComponent === 'quiz' && <Quiz />}
-            {currentComponent === 'lastScreen' && <LastScreen />}
+            <QuizContext.Provider value={{ currentComponent, setCurrentComponent }}>
+                {currentComponent === 'menu' && <MainMenu />}
+                {currentComponent === 'quiz' && <Quiz />}
+                {currentComponent === 'lastScreen' && <LastScreen />}
+            </QuizContext.Provider>
         </div>
     );
 }
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuizContext } from '../Helpers/Contexts'
 
 export default function MainMenu() {
-  return (
-    <div>MainMenu</div>
-  )
+    // eslint-disable-next-line
+    const { currentComponent, setCurrentComponent } = useContext(QuizContext);
+
+    return (
+        <div className='Menu'>
+            <button
+                onClick={() => {
+                    setCurrentComponent('quiz')
+                }}
+            >
+                Start Quiz
+            </button>
+        </div>
+    )
 }
